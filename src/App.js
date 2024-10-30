cb8c18d9a7cb795555dfc3d8b2ae6f15d765ebc3
ElifAkin11 <165787747+ElifAkin11@users.noreply.github.com>
2024-10-30 17:52:27 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="login-container">
+      <div className="login-overlay"></div>
+      <div className="login-card"></div>
+    </div>
+  );
+}
